0a14401446d695449076777128cf9ac6d78c957c
Joevan Zhao <tojoevan@gmail.com>
2016-11-02 16:47:11 +0800

    create sample source

---
+++ php-laravel-mysql-sample-master/app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+use App\Contact;
+
+
+Route::get('/', function () {
+    $isInstall = Schema::hasTable('contacts');
+    if (!$isInstall) {
+
+        Artisan::call('migrate', ['--force' => true]);
+        Artisan::call('db:seed', ['--force' => true]);
+    }
+
+
+    $contacts = Contact::all();
+
+    return view('contacts')
+        ->with('contacts', $contacts);
+});
+
+Route::post('/', function () {
+    Contact::create(Input::all());
+
+    return back();
+});
+
+Route::get('/{id}/delete', function ($id) {
+    $contact = Contact::find($id);
+
+    $contact->delete();
+
+    return back();
+});
